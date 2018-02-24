b406ada2f9d603563c84e38ba229e23a37d851aa
GatesKennedy <GatesKennedy@users.noreply.github.com>
2018-02-24 12:57:02 -0800

    Modify Challenge Xmen Battle Count final submit

--- ChallengeForXmenBattleCount/ChallengeForXmenBattleCount/Default.aspx
+++ ChallengeForXmenBattleCount/ChallengeForXmenBattleCount/Default.aspx
@@ -4,9 +4,18 @@
 
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head runat="server">
-    <title></title>
+    <title>Challenge X-Men Battle Count</title>
+
+    <style>
+        body {
+            background-color: #292929;
+            color: #f9f9f9;
+            font-family: Arial, Helvetica, sans-serif
+        }
+    </style>
 </head>
 <body>
+    <h2>Challenge X-Men Battle Count</h2>
     <form id="form1" runat="server">
     <div>
     
--- ChallengeForXmenBattleCount/ChallengeForXmenBattleCount/Default.aspx.cs
+++ ChallengeForXmenBattleCount/ChallengeForXmenBattleCount/Default.aspx.cs
@@ -11,18 +11,70 @@ namespace ChallengeForXmenBattleCount
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            // Display
+                // "Most Battles Belong to: Name (Value: X)"
+                // "Least Battles Belong to: Name (Value: Y)"
+            
             // Wolverine fewest battles
             // Pheonix most battles
 
-            string[] names = new string[] { "Professor X", "Iceman", "Angel", "Beast", "Pheonix", "Cyclops", "Wolverine", "Nightcrawler", "Storm", "Colossus" };
-            int[] numbers = new int[] { 7, 9, 12, 15, 17, 13, 2, 6, 8, 13 };
+            //Provided Arrays (added 'Shirly Mason' and 'Sybil' for a generalized scenario)
+            string[] names = new string[] { "Professor X", "Iceman", "Angel", "Beast", "Pheonix", "Cyclops", "Wolverine", "Nightcrawler", "Storm", "Colossus", "Shirly Mason", "Sybil" };
+            int[] numbers = new int[] { 7, 9, 12, 15, 17, 13, 2, 6, 8, 13, 17, 2};
 
+            // Define Max and Min
+            int numbersMax = numbers.Max();
+            int numbersMin = numbers.Min();
+
+            // Status for multiple Max, Min Values
+            bool foundMax = false;
+            bool foundMin = false;
+
+            // Declare Result String
             string result = "";
 
+            // Iterate through 'numbers' Array
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                // Compare iteration to Max Value
+                if (numbers[i] == numbersMax)
+                {
+                    // First max-found
+                    if (foundMax==false)
+                    {
+                        result += String.Format("Most Battles Belong to: {0} (Value: {1} Battles)<br />", names[i], numbers[i]);
+                    }
+                    // Succeeding max-founds
+                    else
+                    {
+                        result += String.Format("Most Battles Also Belongs to: {0} (Value: {1} Battles)<br />", names[i], numbers[i]);
+                    }
 
-            // Your Code Here!
+                    foundMax = true;
+                }
+                // Compare iteration to Min Value
+                else if (numbers[i] == numbersMin)
+                {
+                    // First min-found
+                    if (foundMin==false)
+                    {
+                        result += String.Format("Least Battles Belong to: {0} (Value: {1} Battles)<br />", names[i], numbers[i]);
+                    }
+                    // Succeeding min-founds
+                    else
+                    {
+                        result += String.Format("Least Battles Also Belongs to: {0} (Value: {1} Battles)<br />", names[i], numbers[i]);
+                    }
 
+                    foundMin = true;
+                }
+                else
+                {
+                    ; // do nothing. Included for potential break point
+                }
+            }
 
+            // Display Result String
             resultLabel.Text = result;
         }
     }
