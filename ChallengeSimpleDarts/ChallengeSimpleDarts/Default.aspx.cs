79fc94bde445a0483e177646e47ebf81217f5012
GatesKennedy <GatesKennedy@users.noreply.github.com>
2018-03-08 18:58:47 -0800

    Add: ChallengeSimpleDarts

---
+++ ChallengeSimpleDarts/ChallengeSimpleDarts/Default.aspx.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Darts;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ChallengeSimpleDarts
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Random rando1 = new Random();
+            Random rando2 = new Random();
+            Game game = new Game();
+
+            while (game.ScorePlayerOne < 300 && game.ScorePlayerTwo < 300)
+            {
+                int i = 1;
+                game.Round(rando1, rando2);
+                resultLabel.Text += string.Format(".:Round {0}:.<br />Player 1: {1}<br />Player 2: {2}<br /><br />",
+                    i,game.ScorePlayerOne,game.ScorePlayerTwo);
+                i++;
+            }
+            if (game.ScorePlayerOne > game.ScorePlayerTwo) resultLabel.Text += "Player 1 Wins";
+            else resultLabel.Text += "Player 2 Wins";
+        }
+    }
+
+    public class Game
+    {
+        public int ScorePlayerOne { get; set; }
+        public int ScorePlayerTwo { get; set; }
+
+        public Game()
+        {
+            this.ScorePlayerOne = 0;
+            this.ScorePlayerTwo = 0;
+        }
+
+        public void Round(Random rando1, Random rando2)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                ScorePlayerOne += Score.Throw(Dart.Throw(rando1.Next(20), rando2.Next(20)));
+                ScorePlayerTwo += Score.Throw(Dart.Throw(rando1.Next(20), rando2.Next(20)));
+            }
+        }
+    }
+
+    public static class Score
+    {
+        public static int Throw(int[] regionAndRing)
+        {
+            int points = 0;
+            if (regionAndRing[0] == 0) points = 25 * regionAndRing[1];
+            else points = regionAndRing[0] * regionAndRing[1];
+            return points;
+        }
+    }
+}
---
+++ ChallengeSimpleDarts/Darts/Darts.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Darts
+{
+    public static class Dart
+    {
+        /*
+        // Properties
+        public int RandomRegionNumber { get; set; }
+        public int RandomRingNumber { get; set; }
+
+        // Class Constructor
+        public Dart(int _randoRegion, int _randoRing)
+        {
+            int RandomRegionNumber = _randoRegion;
+            int RandomRingNumber = _randoRing;
+        }
+        */
+        
+        // Method Throw()
+        public static int[] Throw(int _randoRegion, int _randoRing)
+        {
+            int[] regionAndMultiplier = new int[2] { 0, 0 };
+            regionAndMultiplier[0] = _randoRegion;
+            if (_randoRing > 1) regionAndMultiplier[1] = 1;
+            else if (_randoRing == 0) regionAndMultiplier[1] = 2;
+            else if (_randoRing == 1) regionAndMultiplier[1] = 3;
+            return regionAndMultiplier;
+        }
+        
+    }
+}
